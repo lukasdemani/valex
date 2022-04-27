176932680a82d110d9b94f4db1bf45f6be5916a3
iagod <iago.tostes@cubos.io>
2022-04-27 17:21:47 -0300

    Preparando revisão de código

--- src/schemas/editCardSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const editCardSchema = joi.object({
-    securityCode: joi.string().length(3).required(),
-    password: joi.string().length(4).required()
-});
-
-export default editCardSchema;
--- src/schemas/transactionSquema.ts
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const transactionSchema = joi.object({
-    businessId: joi.number().required(),
-    amount: joi.number().greater(0).required()
-    
-});
-
-export default transactionSchema;
